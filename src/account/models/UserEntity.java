6124a5bf4be30dde62b2b823946123e8a784e695
Ruslan <gren.ruslan@gmail.com>
2023-07-27 16:18:01 +0300

    add UserEntity

---
+++ src/account/models/UserEntity.java
@@ -0,0 +1,20 @@
+package account.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    private String password;
+}
