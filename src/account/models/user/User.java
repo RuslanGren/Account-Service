45592c870b31bf54ca4822e309b74610ba7c3486
Ruslan <gren.ruslan@gmail.com>
2023-08-15 12:28:13 +0300

    add

--- src/account/models/user/User.java
+++ src/account/models/user/User.java
@@ -24,7 +24,9 @@ public class User {
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     @JsonIgnore
     private List<Role> roles = new ArrayList<>();
