2035d73032e2a298b08d8d574da8a9b77bc9ba7f
Ruslan <gren.ruslan@gmail.com>
2023-09-13 21:13:50 +0300

    update

--- src/account/exceptions/UserIsLockedException.java
+++
@@ -1,8 +0,0 @@
-package account.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "User is locked!")
-public class UserIsLockedException extends RuntimeException {
-}
